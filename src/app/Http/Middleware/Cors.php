74766b9881a05537a7043ff8114c6e0844865f52
Steven Pardo <dspardo6@gmail.com>
2019-10-07 00:48:33 -0500

    Auth header

--- src/app/Http/Middleware/Cors.php
+++ src/app/Http/Middleware/Cors.php
@@ -9,6 +9,6 @@ class Cors {
         return $next($request)
         ->header('Access-Control-Allow-Origin', '*')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
